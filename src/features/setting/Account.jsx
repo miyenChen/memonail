9eb408ba6090bef2fb538b6044132ad9e5e83408
miyen <jorg77jorg77@gmail.com>
2024-01-18 23:03:36 +0800

    feat: Add basic information to setting

--- src/features/setting/Account.jsx
+++ src/features/setting/Account.jsx
@@ -1,13 +1,45 @@
 import Form from '../../ui/Form';
 import FormTitle from '../../ui/FormTitle';
 import Section from '../../ui/Section';
+import InputGroup from '../../ui/InputGroup';
+import Button from '../../ui/Button';
+import styled from 'styled-components';
 
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    & p:first-child {
+        width: 4rem;
+    }
+`;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    gap: 0.5rem;
+`;
 function Account() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
         <>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormTitle>帳戶管理</FormTitle>
-                <Section></Section>
+                <Section>
+                    <Wrapper>
+                        <Flex>
+                            <p>密碼</p>
+                            <Button $size="small">更改密碼</Button>
+                        </Flex>
+                        <Flex>
+                            <p>信箱</p>
+                            <p>example@gmail.com</p>
+                        </Flex>
+                    </Wrapper>
+                </Section>
             </Form>
         </>
     );
--- src/features/setting/Profile.jsx
+++ src/features/setting/Profile.jsx
@@ -1,13 +1,67 @@
+import { useState } from 'react';
+import styled from 'styled-components';
 import Form from '../../ui/Form';
 import FormTitle from '../../ui/FormTitle';
 import Section from '../../ui/Section';
+import Button from '../../ui/Button';
+import Input from '../../ui/Input';
 
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    & p:first-child {
+        width: 4rem;
+    }
+`;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    gap: 1rem;
+`;
 function Profile() {
+    const [name, setName] = useState('小美');
+    const [gender, setGender] = useState('male'); // 初始狀態為'male'
+
+    const genderOptions = [
+        { value: 'male', label: '男' },
+        { value: 'female', label: '女' },
+        { value: 'undisclosed', label: '不透露' },
+    ];
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
     return (
         <>
             <Form>
                 <FormTitle>個人檔案</FormTitle>
-                <Section></Section>
+                <Section>
+                    <Wrapper>
+                        <Flex>
+                            <p>用戶名</p>
+                            <Input value={name} onChange={(e) => setName(e.target.value)} />
+                        </Flex>
+                        <Flex>
+                            <p>性別</p>
+                            {genderOptions.map((option) => (
+                                <label key={option.value}>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value={option.value}
+                                        checked={gender === option.value}
+                                        onChange={handleGenderChange}
+                                    />
+                                    {option.label}
+                                </label>
+                            ))}
+                        </Flex>
+                        <Button $w100>更新個人檔案</Button>
+                    </Wrapper>
+                </Section>
             </Form>
         </>
     );
